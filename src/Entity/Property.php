d1ede9728fe14dcfbba639926dd02e1e4ec29a29
fayçal <faycaldali13@gmail.com>
2019-01-16 10:52:22 +0100

    crud option

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
@@ -94,12 +96,18 @@ class Property
     private $created_at;
 
     /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Option", mappedBy="properties")
+     */
+    private $options;
+
+    /**
      * Property constructor.
      * @throws \Exception
      */
     public function __construct()
     {
         $this->created_at = new \DateTime();
+        $this->options = new ArrayCollection();
     }
 
 
@@ -272,6 +280,34 @@ class Property
     public function setCreatedAt(\DateTimeInterface $created_at): self
     {
         $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Option[]
+     */
+    public function getOptions(): Collection
+    {
+        return $this->options;
+    }
+
+    public function addOption(Option $option): self
+    {
+        if (!$this->options->contains($option)) {
+            $this->options[] = $option;
+            $option->addProperty($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOption(Option $option): self
+    {
+        if ($this->options->contains($option)) {
+            $this->options->removeElement($option);
+            $option->removeProperty($this);
+        }
 
         return $this;
     }
